db7e30b5bb45ce2f4fbb68a49683dff5cf41e9ba
GustavoHenriqueP <gustavo_henriquedepaula@hotmail.com>
2022-11-07 00:06:10 -0300

    Commit 3

--- lib/repositories/disciplina_repository.dart
+++ lib/repositories/disciplina_repository.dart
@@ -4,49 +4,49 @@ import 'package:flutter_aula_1/models/disciplina.dart';
 class DisciplinaRepository {
   static List<Disciplina> tabela = [
     Disciplina(
-      cod: 1,
+      cod: 0,
       cor: Colors.primaries[0], 
       nome: 'Segurança da Informação', 
       professor: 'André Luiz', 
       descricao: ''
     ),
     Disciplina(
-      cod: 2,
+      cod: 1,
       cor: Colors.primaries[3], 
       nome: 'Programação para Dispositivos Móveis', 
       professor: 'José William', 
       descricao: ''
     ),
     Disciplina(
-      cod: 3,
+      cod: 2,
       cor: Colors.primaries[5], 
       nome: 'Programação Linear', 
       professor: 'Carlos Fragoso', 
       descricao: ''
     ),
     Disciplina(
-      cod: 4,
+      cod: 3,
       cor: Colors.primaries[1], 
       nome: 'Engenharia de Software III', 
       professor: 'Simone', 
       descricao: ''
     ),
     Disciplina(
-      cod: 5,
+      cod: 4,
       cor: Colors.primaries[8], 
       nome: 'Inglês V', 
       professor: 'Elenir', 
       descricao: ''
     ),
     Disciplina(
-      cod: 6,
+      cod: 5,
       cor: Colors.primaries[17], 
       nome: 'Laboratório de Banco de Dados', 
       professor: 'Marcio', 
       descricao: ''
     ),
     Disciplina(
-      cod: 7,
+      cod: 6,
       cor: Colors.primaries[12], 
       nome: 'Sistemas Operacionais II', 
       professor: 'Helder', 
