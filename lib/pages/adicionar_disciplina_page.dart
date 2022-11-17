838e4574473794f4a1e41a2d2a4fe9c6cacde512
LeyPoiani <leianny.poiani@gmail.com>
2022-11-16 21:33:10 -0300

    Add a new file adicionar_disciplina_page

---
+++ lib/pages/adicionar_disciplina_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class AdicionarDisciplinaPage extends StatefulWidget {
+  const AdicionarDisciplinaPage({super.key});
+
+
+  @override
+  State<AdicionarDisciplinaPage> createState() => _AdicionarDisciplinaPageState();
+}
+
+class _AdicionarDisciplinaPageState extends State<AdicionarDisciplinaPage> {
+  final _form = GlobalKey<FormState>(); // Gera uma key (identificador) para o formulário
+
+  final _nome = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Adicionar disciplina'
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(32),
+          child: SingleChildScrollView(
+            reverse: true,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Form(
+                  key: _form,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        maxLines: null,
+                        controller: _nome,
+                        style: TextStyle(
+                          fontSize: 18
+                        ),
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(16))
+                          ),
+                          labelText: 'Nome'
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 14),
+                        child: TextFormField(
+                          maxLines: null,
+                          controller: _nome,
+                          style: TextStyle(
+                            fontSize: 18
+                          ),
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(Radius.circular(16))
+                            ),
+                            labelText: 'Professor'
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.purple[800]
+                  ),
+                  alignment: Alignment.bottomCenter,
+                  margin: EdgeInsets.only(top: 24),
+                  child: ElevatedButton(
+                    onPressed: null,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(padding: EdgeInsets.all(16),
+                        child: Text(
+                          'SALVAR',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18
+                          ),
+                        ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
--- lib/pages/disciplinas_page.dart
+++ lib/pages/disciplinas_page.dart
@@ -5,6 +5,8 @@ import 'package:flutter_aula_1/repositories/listar_tarefas_repository.dart';
 import 'package:flutter_aula_1/repositories/disciplina_repository.dart';
 import 'package:provider/provider.dart';
 
+import 'adicionar_disciplina_page.dart';
+
 class DisciplinasPage extends StatefulWidget { ////StatefulWidget - Um Widget mutável
 
   const DisciplinasPage({Key? key}) : super(key: key);
@@ -48,11 +50,22 @@ class _DisciplinasPageState extends State<DisciplinasPage> {
     return buffer.toString();
   }
 
+adicionarDisciplina() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => AdicionarDisciplinaPage(),
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) { //Método build cria o widget em si
     return Scaffold( //Scaffold serve para formatar nossa tela em um MaterialApp
       appBar: AppBar(
         title: Text('Disciplinas'),
+        actions: [
+          IconButton(onPressed: adicionarDisciplina, icon: Icon(Icons.add))
+        ],
       ),
       body: ListView.separated( //Corpo do App - Neste caso, uma ListView
         itemBuilder: (BuildContext context, int disciplina) {
